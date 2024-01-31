eeaa1a0ce400f21e216ddd4034abb45194af62bc
Manas Jain Kuniya <138317061+this-is-mjk@users.noreply.github.com>
2024-01-31 20:58:26 +0530

    Add files via upload

---
+++ CF/model.go
@@ -0,0 +1,33 @@
+package model
+
+type Actions []Action
+
+type BE struct {
+	Title                   string   `json:"title"`
+	Content                 string   `json:"content"`
+	Id                      int      `json:"id"`
+	OriginalLocale          string   `json:"originalLocale"`
+	CreationTimeSeconds     int      `json:"creationTimeSeconds"`
+	AuthorHandle            string   `json:"authorHandle"`
+	Locale                  string   `json:"locale"`
+	ModificationTimeSeconds int      `json:"modificationTimeSeconds"`
+	AllowViewHistory        bool     `json:"allowViewHistory"`
+	Tags                    []string `json:"tags"`
+	Rating                  int      `json:"rating"`
+}
+
+type C struct {
+	Text                string `json:"text"`
+	Rating              int    `json:"rating"`
+	Id                  int    `json:"id"`
+	CreationTimeSeconds int    `json:"creationTimeSeconds"`
+	CommentatorHandle   string `json:"commentatorHandle"`
+	Locale              string `json:"locale"`
+	ParentCommentId     int    `json:"parentCommentId"`
+}
+
+type Action struct {
+	TimeSeconds int64   `json:"timeSeconds"`
+	BlogEntry   *BE `json:"blogEntry"`
+	Comment     *C   `json:"comment"`
+}
